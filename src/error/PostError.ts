2bb99a0da663d25427b4f8f3f5329e4d2c82eb77
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2023-02-02 20:19:50 -0300

    Fixing typos

--- src/error/PostError.ts
+++ src/error/PostError.ts
@@ -1,7 +1,13 @@
-import { CustomError } from "./CustomError";
+import { CustomError } from "./CustomError"
 
-export class InvalidType extends CustomError{
-    constructor(){
+export class InvalidType extends CustomError {
+    constructor() {
         super(400, "Inform 'normal' or 'event' for post type.")
+    }
+}
+
+export class PostNotFound extends CustomError {
+    constructor() {
+        super(404, "Post not found.")
     }
 }
