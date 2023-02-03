50581524ee97d428d1e90ff4def947457c022cdd
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2023-02-02 21:43:16 -0300

    Adds getID methods

--- src/model/Friendship.ts
+++ src/model/Friendship.ts
@@ -3,4 +3,12 @@ export class Friendship {
         private user1_id:string,
         private user2_id:string
     ){}
+
+    public getUser1Id () {
+        return this.user1_id
+    }
+    
+    public getUser2Id () {
+        return this.user2_id
+    }
 }
